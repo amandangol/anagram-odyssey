0b48146545e25a775b2e05a0b188083bedd4551a
amandangol <icrextha@gmail.com>
2024-08-20 23:15:24 +0545

    vercel deployment :deploy:

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import { getLocalStorageItem, setLocalStorageItem } from './utils/localStorage';
 import ErrorBoundary from './components/ErrorBoundary';
 import 'tailwindcss/tailwind.css';
 
-
 function App() {
   const [darkMode, setDarkMode] = useState(() => {
     return getLocalStorageItem('darkMode') === 'true';
@@ -25,8 +24,8 @@ function App() {
       darkMode ? 'dark bg-gray-900' : 'bg-gradient-to-br from-indigo-100 via-purple-200 to-blue-200'
     }`}>
       <header className={`py-4 px-6 lg:px-8 ${darkMode ? 'bg-gray-800' : 'bg-white'} shadow-md`}>
-        <div className="max-w-7xl mx-auto flex items-center justify-between">
-          <div className="flex items-center">
+        <div className="max-w-7xl mx-auto flex items-center justify-between flex-wrap">
+          <div className="flex items-center flex-shrink-0">
             <h1 className="text-2xl sm:text-3xl font-bold tracking-tight relative">
               <span className={`${darkMode ? 'text-teal-300' : 'text-teal-600'}`}>
                 Anagram
@@ -37,8 +36,8 @@ function App() {
               <span className="absolute -top-1 -right-6 text-2xl sm:text-3xl animate-pulse">🔎</span>
             </h1>
           </div>
-          
-          <div className="flex items-center">
+
+          <div className="flex items-center space-x-4 mt-2 lg:mt-0">
             <button
               onClick={toggleDarkMode}
               className={`p-2 rounded-full ${
@@ -47,6 +46,7 @@ function App() {
             >
               {darkMode ? <FaSun size={20} /> : <FaMoon size={20} />}
             </button>
+        
           </div>
         </div>
       </header>
@@ -58,12 +58,12 @@ function App() {
         </div>
       </main>
       <footer
-         className={`py-6 ${
+        className={`py-6 ${
           darkMode ? 'bg-gray-800 text-gray-300' : 'bg-gray-100 text-gray-700'
         }`}
       >
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="flex flex-col md:flex-row justify-center items-center flex space-x-4">
+          <div className="flex flex-col md:flex-row justify-center items-center space-x-0 md:space-x-4">
             <p className="mb-4 md:mb-0">&copy; 2024 Anagram Odyssey by 4m.4n. All rights reserved.</p>
             <div className="flex space-x-4">
               <a href="https://github.com/amandangol" target="_blank" rel="noopener noreferrer" className={`hover:text-indigo-500 transition-colors duration-300 ${darkMode ? 'text-gray-300' : 'text-gray-600'}`}>
